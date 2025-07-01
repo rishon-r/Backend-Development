038f556988631aa41fd8bce83e03a329a301c6f6
Rishon <rishonrajesh13@gmail.com>
2025-07-01 09:20:39 -0700

    Project 2: Styling added

--- project_2/src/server.js
+++ project_2/src/server.js
@@ -1,11 +1,19 @@
 // IMPORTING EXPRESS in a different way than the one we used in project 1
 // This is the modern and recommended one
-import express from 'express'
+import express from 'express';
+import path, {dirname} from 'path';
+import {fileURLTOPath} from 'url';
 
 // INITIALIZING WEB SERVER INSTANCE
 const app=express(); 
 
 const PORT= process.env.PORT || 8080;
+
+// 
+
+app.get('/', (req,res) => {
+    res.sendFile
+});
 
 app.listen(PORT,() => {
   console.log(`Server has started on port: ${PORT}`);
