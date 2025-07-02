bc06e67bdffab1779e352c12f4003d5ffdbefd0b
Rishon <rishonrajesh13@gmail.com>
2025-07-02 12:24:29 -0700

    Project 2: Log in feature and middleware added

--- project_2/src/routes/authRoutes.js
+++ project_2/src/routes/authRoutes.js
@@ -34,6 +34,9 @@ router.post('/register', (req, res) => {
   const hashedPassword = bcrypt.hashSync(password, 8);
 
   // Save the new user and hashed password to the database
+  // When interacting with the database, it always makes sense to use a try and catch
+  // as the dtabase might sometimes throw unwarranted errors
+  // and we obviously don't want these unwanted errors crashing our system
   try {
     // Prepare an SQL statement to insert the new user into the 'users' table
     // In libraries like better-sqlite3, db.prepare() is used to create a reusable prepared statement
@@ -92,6 +95,31 @@ router.post('/register', (req, res) => {
 // This endpoint will handle user login (currently empty).
 
 router.post('/login', (req, res) => {
+   // we get their email, and we look up the password associated with that email in the database
+    // but we get it back and see it's encrypted, which means that we cannot compare it to the one the user just used trying to login
+    // so what we can to do, is again, one way encrypt the password the user just entered
+
+    const { username, password } = req.body
+
+    try {
+        const getUser = db.prepare('SELECT * FROM users WHERE username = ?')
+        const user = getUser.get(username)
+
+        // if we cannot find a user associated with that username, return out from the function
+        if (!user) { return res.status(404).send({ message: "User not found" }) }
+
+        const passwordIsValid = bcrypt.compareSync(password, user.password)
+        // if the password does not match, return out of the function
+        if (!passwordIsValid) { return res.status(401).send({ message: "Invalid password" }) }
+        console.log(user)
+
+        // then we have a successful authentication
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '24h' })
+        res.json({ token })
+    } catch (err) {
+        console.log(err.message)
+        res.sendStatus(503)
+    }
   
 });
 
--- project_2/src/routes/todoRoutes.js
+++ project_2/src/routes/todoRoutes.js
@@ -5,6 +5,9 @@ const router= express.Router();
 
 // Get all to-do's for logged in user
 router.get('/', (req,res) => {
+  const getTodos = db.prepare('SELECT * FROM todos WHERE user_id = ?');
+  const todos = getTodos.all(req.userId);
+  res.json(todos);
 
 });
 
